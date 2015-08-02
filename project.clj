43f648b98854f2e933ed51fdf371b935a92aa84e
Roberto Guerra <uris77@gmail.com>
2015-08-02 12:16:22 -0600

    Adds environ.

--- project.clj
+++ project.clj
@@ -2,12 +2,14 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.7.0"]
+                 [environ "1.0.0"]
                  [compojure "1.4.0"]
                  [hiccup "1.0.5"]
                  [ring-server "0.4.0"]
                  [com.cemerick/friend "0.2.1" :exclusions [ring/ring-core org.clojure/core.cache org.apache.httpcomponents/httpclient]]
                  [friend-oauth2 "0.1.3" :exclusions [commons-logging org.apache.httpcomponents/httpcore]]]
-  :plugins [[lein-ring "0.9.6"]]
+  :plugins [[lein-ring "0.9.6"]
+            [lein-environ "1.0.0"]]
   :ring {:handler pasmo-gigi.handler/app
          :init    pasmo-gigi.handler/init
          :destroy pasmo-gigi.handler/destroy}
