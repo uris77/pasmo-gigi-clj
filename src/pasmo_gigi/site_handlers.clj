1e0f4a2011c08c17710d458af11d8ff10eab1d6f
Roberto Guerra <uris77@gmail.com>
2015-08-18 13:46:44 -0600

    Use correct role for /app

--- src/pasmo_gigi/site_handlers.clj
+++ src/pasmo_gigi/site_handlers.clj
@@ -14,9 +14,9 @@
 (defroutes site-routes
   (GET "/login" req (main-handler))
   (GET "/hello" req
-       "HELLO" (friend/authorize #{:pasmo-gigi.auth-config/user} (main-handler)))
+       (friend/authorize #{:pasmo-gigi.auth-config/user} (main-handler)))
   (GET "/app" req
-       (friend/authorize #{:user} (main-handler)))
+       (friend/authorize #{:pasmo-gigi.auth-config/user} (main-handler)))
   (GET "/oauth2callback" req
        (friend/authorize #{:pasmo-gigi.auth-config/user} (main-handler)))
   (friend/logout (ANY "/logout" request (response/redirect "/")))
